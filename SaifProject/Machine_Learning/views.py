c0fa075f4463a77c81111566a0c57613d1a2cf51
saifndub <saif.ndub@gmail.com>
2023-03-15 16:41:37 +0600

    Add variables in HTML

--- SaifProject/Machine_Learning/views.py
+++ SaifProject/Machine_Learning/views.py
@@ -3,13 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def mlearning(request):
-    return render(request, 'machine_learning/machine_learning.html')
-
-def dtree(request):
-    return render(request, 'machine_learning/DT.html')
+    offering = {'what' : 'which is interesting subject.'}
+    return render(request, 'machine_learning/machine_learning.html', context=offering)
 
 def random(request):
     return render(request, 'machine_learning/random_forest.html')
+
+def dtree(request):
+    return render(request, 'machine_learning/DT.html')
 
 def k_nearest(request):
     return render(request, 'machine_learning/knn.html')
